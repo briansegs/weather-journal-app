7e2d305bc39132aae7aa92de34c5778186fbafb1
brian <briansegs@gmail.com>
2021-03-09 15:25:06 -0500

    fix: correct baseURL

--- website/app.js
+++ website/app.js
@@ -1,16 +1,18 @@
 /* Global Variables */
 
-let baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 let apiKey = ',us&appid=73ce595a0ec8725af0a3bf9dfd38dcc4';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-document.getElementById('generate').addEventListener('click', (e) => {
+document.getElementById('generate').addEventListener('click', action);
+
+function action (e) {
     let zip = document.getElementById('zip').value;
     getWeather(baseURL, zip, apiKey)
-});
+}
 
 const getWeather = async (baseURL, zip, apiKey) => {
     const res = await fetch(baseURL+zip+apiKey)
@@ -22,3 +24,7 @@ const getWeather = async (baseURL, zip, apiKey) => {
         console.log('error', error);
     }
 };
+
+
+
+
