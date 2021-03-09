af742dd1ba7b496ebdc9179bd53b0dbe9583b067
brian <briansegs@gmail.com>
2021-03-09 14:30:09 -0500

    feat: Add eventlistener for generate click

--- website/app.js
+++ website/app.js
@@ -7,3 +7,7 @@ let apiKey = '73ce595a0ec8725af0a3bf9dfd38dcc4';
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+document.getElementById('generate').addEventListener('click', (e) => {
+    let zip = document.getElementById('zip').nodeValue;
+    getWeather(baseURL, zip, apiKey)
+});
