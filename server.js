1cdf91f686a749e7df936a4602faacf6770ac5cf
brian <briansegs@gmail.com>
2021-03-09 11:35:36 -0500

    fix: Update to built-in parsers

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
-const bodyParser = require('body-parser')
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
