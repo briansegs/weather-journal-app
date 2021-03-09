f4244d614a63c5e49584b8cbdec66c8ecaf0b0cd
brian <briansegs@gmail.com>
2021-03-09 14:28:40 -0500

    feat: Add global variables

--- website/app.js
+++ website/app.js
@@ -1,5 +1,9 @@
 /* Global Variables */
 
+let baseURL = 'api.openweathermap.org';
+let apiKey = '73ce595a0ec8725af0a3bf9dfd38dcc4';
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
